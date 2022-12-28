83dd29a2d05ccdc37ff1086c07cf9cffa57133fb
jefferfonseca <jefferfonseca@gmail.com>
2022-12-27 19:58:36 -0500

    perfil toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
+//agregado por mi
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/page-not-found', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent } // error 404
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PerfilComponent,
+    PageNotFoundComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
